a345f4a70dd11a0e0daaa03ccc71c6fb5fb420fe
Lucas-Kato <118133248+Lucas-Kato@users.noreply.github.com>
2022-11-19 12:32:20 -0300

    Inserindo types da Aula1
    Tipo number / flutuante / NaN / String / Boolean.

---
+++ JavaScript-para-Back-End/type-string.js
@@ -0,0 +1,17 @@
+//Tipo String
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'O leo disse "oi!"';
+const citacao1 = "O leo disse'Oi!'";
+const citacao2 = "Meu nome é " 
+const meuNome = "Kato";
+
+//contatenação (+)
+
+console.log(citacao2 + meuNome)
+
+
