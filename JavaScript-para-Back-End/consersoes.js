5b10af6934daf8b01c9e6aa8346bbcf4ef02839a
Lucas Kato <lucaskato550@gmail.com>
2022-11-19 15:44:34 -0300

    Aula2
    Aprendendo script / true ou false / variável/ conversão.

---
+++ JavaScript-para-Back-End/consersoes.js
@@ -0,0 +1,16 @@
+// Tipo de dado
+//booleanos
+
+// conversão impícita
+const numero = 789;
+const numeroString = Number("456a")
+console.log(numero == numeroString);
+console.log(numero === numeroString);
+
+//Numer()
+//String()
+console.log(numero + Number(numeroString));
+
+console.log(numero + numeroString);
+
+//convesão explícita
