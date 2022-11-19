00b93694f05b4a0367a9cc1b75e1e976f13ca4d7
Lucas Kato <lucaskato550@gmail.com>
2022-11-19 17:59:00 -0300

    Aura 2 finalização.

--- JavaScript-para-Back-End/consersoes.js
+++ JavaScript-para-Back-End/consersoes.js
@@ -14,3 +14,37 @@ console.log(numero + Number(numeroString));
 console.log(numero + numeroString);
 
 //convesão explícita
+
+
+let telefone = 12341234;
+console.log("O telefone é" + String(telefone));
+
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString());
+
+let usuarioConectado = false;
+console.log(String(usuarioConectado));
+
+usuarioConectado = true;
+console.log(String(usuarioConectado));
+
+//Number
+let largura = "10";
+let altura = "5";
+
+console.log(Number(largura)*Number(altura));
+
+let largura = "10";
+let altura = "5";
+
+console.log(+ largura * + altura);
+
+let meuNome = "leonardo";
+console.log(Number(meuNome));
+console.log(+ meuNome);
+
+let usuarioConectado = false;
+console.log(Number(usuarioConectado));
+
+usuarioConectado = true;
+console.log(Number(usuarioConectado));
