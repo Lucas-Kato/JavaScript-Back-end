45322b734bd4df31ad986ad25f65d5433c65b5aa
Lucas Kato <lucaskato550@gmail.com>
2022-11-20 22:38:02 -0300

    Aula 4

---
+++ JavaScript-para-Back-End/templateStrings.js
@@ -0,0 +1,9 @@
+const nome = "Kato";
+const idade = 2022 - 1993;
+const estadoDeNascimento = "Paraná";
+
+//const apresentação = "meu nome é " + nome + " , minha idade é " + idade + " e nasci na estado de " + estadoDeNascimento;
+
+const apresentação = `meu nome é ${nome}, minha idade é ${idade} e nasci no estado de ${estadoDeNascimento}`
+
+console.log(apresentação);
