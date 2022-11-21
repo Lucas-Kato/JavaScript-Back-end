2a00a6f1bfb155804532f7ffd87727fa5848d96b
Lucas Kato <lucaskato550@gmail.com>
2022-11-21 14:48:26 -0300

    Aula 5
    Funções / Parâmetros e argumentos / Expressão de função / Arrow Function.

---
+++ JavaScript-para-Back-End/arrwFunction.js
@@ -0,0 +1,22 @@
+function aprensentar(nome) {
+    return `Meu nome é ${nome}`;
+}
+
+//Arrow Function
+const apresentarArrow = nome => `Meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+// Arrow Function com + de 1 linha de instrução
+
+const somaNumerosPequenos = (num1, num2) => {
+    if (num1 > 10 || num2 > 10) {
+        return "somnete números de 1 a 9"
+    }else{
+        return num1 + num2;
+    }
+}
+
+// HOISTING: arrow functio se comporta como expressão
+
+//operador maior ou igual que
+// >=
