e916e18b6af92dfd904c65a41efa4271114d9583
Lucas Kato <lucaskato550@gmail.com>
2022-11-27 11:43:17 -0300

    Organização

--- JavaScript-para-Back-End/AlgorimosDeIrdenação/Aula1/Aula2/selectionSort.js
+++ JavaScript-para-Back-End/AlgorimosDeIrdenação/Aula1/Aula2/selectionSort.js
@@ -6,16 +6,15 @@ for (let atual = 0; atual < livros.length -1; atual ++) {
     let menor = menorValor(livros, atual);
     let livroAtual = livros[atual];
 
-    console.log("posição atual", atual);
-    console.log("livro atual", livros[atual]);
     let livroMenorPreco = livros[menor];
-
-
-    console.log("livro menor preço", livros[menor]);
 
     livros[atual] = livroMenorPreco;
     livros[menor] = livroAtual;
 
+
+    console.log("livro menor preço", livros[menor]);
+    console.log("posição atual", atual);
+    console.log("livro atual", livros[atual]);
  }
 
  console.log(livros);
